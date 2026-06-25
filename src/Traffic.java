c74104b9518c7c2e67837977b66ebcdf19574603
Sarumathi S <maheshcharumathi1@gmail.com>
2026-06-25 10:14:25 +0530

    Traffic project created

---
+++ src/Traffic.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Traffic{
+    public static void main(String [] args){
+        Scanner Scanner=new Scanner(System.in);
+        System.out.print("Enter the colour");
+        String colour=Scanner.nextLine();
+        switch(colour){
+            case "Red":
+            {
+                System.out.print("STOP");
+                break;
+            }
+            case "Green":
+            {
+                System.out.print("GO");
+                break;
+            }
+            case "Yellow":
+            {
+                System.out.print("GET READY");
+                break;
+            }
+            default:
+            {
+                System.out.print("Invalid colour");
+            }
+        }
+    }
+}
+

a755ac3f82186bb9bf42d2127003c6348c16cba9
Sarumathi S <maheshcharumathi1@gmail.com>
2026-06-25 14:19:20 +0530

    java program for days in one week

---
+++ src/Day_6/Day.java
@@ -0,0 +1,14 @@
+package Day_6;
+
+public class Day {
+    public static void main(String [] args){
+        for(int i=0;i<1;i++){
+            for(int day=1;day<=7;day++){
+                System.out.println("day " +day);
+                System.out.println("come to college. study and go to home");
+            }
+            System.out.println();
+        }
+    }
+}
+

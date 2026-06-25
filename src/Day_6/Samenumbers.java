916ffd5c4acb889df729a499e4bafb4a361a41e4
Sarumathi S <maheshcharumathi1@gmail.com>
2026-06-25 13:47:48 +0530

    java program for nested loop in same repeated numbers

---
+++ src/Day_6/Samenumbers.java
@@ -0,0 +1,13 @@
+package Day_6;
+
+public class Samenumbers {
+    public static void main(String [] args){
+        for(int i=1;i<5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+

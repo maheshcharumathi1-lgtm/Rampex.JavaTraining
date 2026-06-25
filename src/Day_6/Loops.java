363fdef0ad86f13024aa6cc034d96f0b0c2573b9
Sarumathi S <maheshcharumathi1@gmail.com>
2026-06-25 13:52:58 +0530

    java program for nested loops

--- src/Day_6/Loops.java
+++ src/Day_6/Loops.java
@@ -1,6 +1,6 @@
 package Day_6;
 
-public class Samenumbers{
+public class Loops {
     public static void main(String [] args){
         for(int i=1;i<5;i++){
             for(int j=1;j<=5;j++){
@@ -10,4 +10,5 @@ public class Samenumbers{
         }
     }
 }
+
 

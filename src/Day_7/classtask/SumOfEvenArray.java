3a48fd5cd4db97f0f87a3e6512188d3f467e568a
Sarumathi S <maheshcharumathi1@gmail.com>
2026-06-27 15:45:07 +0530

    Java program for Sum of array

--- src/Day_7/classtask/SumOfEvenArray.java
+++ src/Day_7/classtask/SumOfEvenArray.java
@@ -5,7 +5,7 @@ public class SumOfEvenArray {
         public static void main(String [] args) {
             int [] arr={1,2,3,4,5};
             int sum=0;
-            for(int i=0;i<5;i=i+2){
+            for(int i=0;i<arr.length;i=i+2){
                 sum=sum+arr[i];
             }
             System.out.println(sum);

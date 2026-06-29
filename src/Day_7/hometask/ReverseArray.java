ddce97d5e3437f89659e4d7992095d3540a7d626
Sarumathi S <maheshcharumathi1@gmail.com>
2026-06-29 09:19:38 +0530

    java program for reverse

---
+++ src/Day_7/hometask/ReverseArray.java
@@ -0,0 +1,14 @@
+package Day_7.hometask;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.print("Reversed Array: ");
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
